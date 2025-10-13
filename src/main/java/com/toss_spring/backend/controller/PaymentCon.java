7c00ddb02ec764ba1700bb0b3f4571f8259e039a
BumTiger <jbpark03@gmail.com>
2025-10-13 14:15:01 +0900

    add prefix to orderId came thru' amount save request

--- src/main/java/com/toss_spring/backend/controller/PaymentCon.java
+++ src/main/java/com/toss_spring/backend/controller/PaymentCon.java
@@ -1,6 +1,7 @@
 package com.toss_spring.backend.controller;
 
 import com.toss_spring.backend.request.SaveAmountReq;
+import com.toss_spring.backend.util.OrderIdPrefixGen;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,9 @@ public class PaymentCon {
   public ResponseEntity<?> saveAmountTemporarily(
       HttpSession session, @RequestBody SaveAmountReq request) {
 
-    session.setAttribute(request.getOrderId(), request.getAmount());
+    String prefix = OrderIdPrefixGen.getOrderIdPrefix(6);
+
+    session.setAttribute(prefix + request.getOrderId(), request.getAmount());
     return ResponseEntity.ok("세션에 <주문 ID, 지불 금액> 항목을 저장함.");
   }
 }
