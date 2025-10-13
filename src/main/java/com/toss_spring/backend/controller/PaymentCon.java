c331c2b6710831db7f7e2c86ce73b7f6a3f7fdf7
BumTiger <jbpark03@gmail.com>
2025-10-13 16:38:27 +0900

    display id prefix thru logging

--- src/main/java/com/toss_spring/backend/controller/PaymentCon.java
+++ src/main/java/com/toss_spring/backend/controller/PaymentCon.java
@@ -3,22 +3,26 @@ package com.toss_spring.backend.controller;
 import com.toss_spring.backend.request.SaveAmountReq;
 import com.toss_spring.backend.util.OrderIdPrefixGen;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentCon {
+  private static final Logger logger = LoggerFactory.getLogger(PaymentCon.class);
+
   @PostMapping("/saveAmount")
   public ResponseEntity<?> saveAmountTemporarily(
       HttpSession session, @RequestBody SaveAmountReq request) {
 
-    String prefix = OrderIdPrefixGen.getOrderIdPrefix(6);
+    var orderId = OrderIdPrefixGen.getOrderIdPrefix(6) + request.getOrderId();
+    logger.info("orderId: " + orderId);
+    session.setAttribute(orderId, request.getAmount());
 
-    session.setAttribute(prefix + request.getOrderId(), request.getAmount());
     return ResponseEntity.ok("세션에 <주문 ID, 지불 금액> 항목을 저장함.");
   }
 }
