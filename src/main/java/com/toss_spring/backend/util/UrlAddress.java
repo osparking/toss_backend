e632be7448b6c5077a6748c1161e8dc0f557413a
BumTiger <jbpark03@gmail.com>
2025-10-22 21:10:50 +0900

    TossPayment member class - URL address

---
+++ src/main/java/com/toss_spring/backend/util/UrlAddress.java
@@ -0,0 +1,14 @@
+package com.toss_spring.backend.util;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class UrlAddress {
+    private String url;
+}
